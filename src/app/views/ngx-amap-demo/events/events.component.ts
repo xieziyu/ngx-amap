10ab6295d91526afd24a0a1bd436becc7b2ec321
Xie, Ziyu <ziyu.xie@foxmail.com>
2018-05-16 14:50:38 +0800

    Fix doc error in amap demo page

--- src/app/views/ngx-amap-demo/events/events.component.ts
+++ src/app/views/ngx-amap-demo/events/events.component.ts
@@ -1,42 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const require: any;
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
-  demo_md_html = `\
-<ngx-amap class="demo-map" [resizeEnable]="true"
-  (complete)="onMapEvent($event, 'complete')"
-  (ready)="onMapEvent($event, 'ready')"
-  (mapmove)="onMapEvent($event, 'mapMove')"
-  (moveStart)="onMapEvent($event, 'moveStart')"
-  (moveEnd)="onMapEvent($event, 'moveEnd')"
-  (zoomChange)="onMapEvent($event, 'zoomChange')"
-  (zoomStart)="onMapEvent($event, 'zoomStart')"
-  (zoomEnd)="onMapEvent($event, 'zoomEnd')"
-  (resize)="onMapEvent($event, 'resize')"
-  (mapClick)="onMapEvent($event, 'mapClick')"
-  (mapDblClick)="onMapEvent($event, 'mapDblClick')"
-  (rightClick)="onMapEvent($event, 'rightClick')"
-  (mouseMove)="onMapEvent($event, 'mouseMove')"
-  (mouseOver)="onMapEvent($event, 'mouseOver')"
-  (mouseWheel)="onMapEvent($event, 'mouseWheel')"
-  (mouseOut)="onMapEvent($event, 'mouseOut')"
-  (mouseUp)="onMapEvent($event, 'mouseUp')"
-  (mouseDown)="onMapEvent($event, 'mouseDown')"
-  (touchStart)="onMapEvent($event, 'touchStart')"
-  (touchMove)="onMapEvent($event, 'touchMove')"
-  (touchEnd)="onMapEvent($event, 'touchEnd')"
-  (hotspotClick)="onMapEvent($event, 'hotspotClick')"
-  (hotspotOver)="onMapEvent($event, 'hotspotOver')"
-  (hotspotOut)="onMapEvent($event, 'hotspotOut')"
-  (dragStart)="onMapEvent($event, 'dragStart')"
-  (dragging)="onMapEvent($event, 'dragging')"
-  (dragEnd)="onMapEvent($event, 'dragEnd')"
-></ngx-amap>
-`;
+  demo_html = require('!!html-loader!./events.component.html');
+
   constructor() { }
 
   ngOnInit() {
